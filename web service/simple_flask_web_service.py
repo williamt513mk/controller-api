34ccd599c431d3e4826a9dd5679aae2615a8abe3
root <cfleu198@gmail.com>
2018-05-15 10:04:05 +0000

    fix unicode and app start issues

--- web service/simple_flask_web_service.py
+++ web service/simple_flask_web_service.py
@@ -1,26 +1,23 @@
-#import flask 這個套件
 from flask import Flask, request, render_template
 app = Flask(__name__)
 import updata_database
 
 #I just want to be able to manipulate the parameters
-#使用者輸/login 如果等於post執行下列 def login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    #確認是否為POST
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         print('POST: input uid & pwd: %s, %s' % (username,password))
 
-    else:#call example: http://127.0.0.1:3361/login?username=flsak&password=12346 #確認是否直接輸入http網址
+    else:#call example: http://127.0.0.1:3361/login?username=flsak&password=12346 
      
-        username = request.args.get('username')#讀取username值
+        username = request.args.get('username')
         
-        password = request.args.get('password')#讀取password值
-        print('GET: input uid & pwd: %s, %s' % (username,password))#列印出GET:input 於DOS畫面
+        password = request.args.get('password')
+        print('GET: input uid & pwd: %s, %s' % (username,password))
         
-    return 'Hello ' + username + password + '!'#於網頁上顯示出結果
+    return 'Hello ' + username + password + '!'
 
 @app.route('/realtimesignal', methods=['GET', 'POST'])
 def realtimesignal():
@@ -28,7 +25,7 @@ def realtimesignal():
         grafana_name = request.form['channel']
         value = request.form['value']
         print('POST: channel=%s' % (channel))
-    else:#call example: http://127.0.0.1:3361/realtimesignal?channel=TPM K1廢氣風扇(西) 背輥/工輥 驅動側軸承&value=1
+    else:#call example: http://127.0.0.1:3361/realtimesignal?channel=TPM K1
         grafana_name = request.args.get('channel')
         value = request.args.get('value')
         print('GET: channel=%s' % (grafana_name))
--- web service/simple_flask_web_service_rts.py
+++ web service/simple_flask_web_service_rts.py
@@ -1,14 +1,9 @@
-#import flask 這個套件
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-
-#使用模塊的connect()功能psycopg2
 import psycopg2
-#以下config()函數讀取database.ini文件並返回連接參數
 from configparser import ConfigParser
 
-#以下config()函數讀取database.ini文件並返回連接參數
 def config(filename='database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
@@ -42,7 +37,6 @@ def connect(channel_name,value=1):
         
         # execute a statement
         print('update realtime_web_service table:')
-        #PG SQL 語法
         #clear all to 0
         SQL = 'UPDATE realtime_web_service.realtime_web_service SET "IfUse" = \'{0}\''.format(0)
         print(SQL)
@@ -56,11 +50,9 @@ def connect(channel_name,value=1):
         #SQL = "SELECT * FROM realtime_web_service.realtime_web_service WHERE channel_name='%s'" % (channel_name)
         #print('perform checking: %s' % SQL)
         cur.execute(SQL)
-        #cur.execute("SELECT * FROM realtime_web_service.realtime_web_service WHERE channel_name='TPM K1廢氣風扇(西) 背輥/工輥 驅動側軸承'")
         # display the PostgreSQL database server version
         #result = cur.fetchall()
         #print(result)
-        #print([n.name for n in cur.description]) #找出欄位名稱
         # close the communication with the PostgreSQL
         print('perform close & commit:')
         cur.close()
@@ -73,23 +65,21 @@ def connect(channel_name,value=1):
             print('Database connection closed.')
  
 #I just want to be able to manipulate the parameters
-#使用者輸/login 如果等於post執行下列 def login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    #確認是否為POST
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         print('POST: input uid & pwd: %s, %s' % (username,password))
 
-    else:#call example: http://127.0.0.1:3361/login?username=flsak&password=12346 #確認是否直接輸入http網址
+    else:#call example: http://127.0.0.1:3361/login?username=flsak&password=12346 #
      
-        username = request.args.get('username')#讀取username值
+        username = request.args.get('username')#
         
-        password = request.args.get('password')#讀取password值
-        print('GET: input uid & pwd: %s, %s' % (username,password))#列印出GET:input 於DOS畫面
+        password = request.args.get('password')#
+        print('GET: input uid & pwd: %s, %s' % (username,password))#
         
-    return 'Hello ' + username + password + '!'#於網頁上顯示出結果
+    return 'Hello ' + username + password + '!'
 
 @app.route('/realtimesignal', methods=['GET', 'POST'])
 def realtimesignal():
@@ -97,7 +87,7 @@ def realtimesignal():
         grafana_name = request.form['channel']
         value = request.form['value']
         print('POST: channel=%s' % (channel))
-    else:#call example: http://127.0.0.1:3361/realtimesignal?channel=TPM K1廢氣風扇(西) 背輥/工輥 驅動側軸承&value=1
+    else:#call example: http://127.0.0.1:3361/realtimesignal?channel=TPM K1
         grafana_name = request.args.get('channel')
         value = request.args.get('value')
         print('GET: channel=%s' % (grafana_name))
@@ -114,4 +104,4 @@ def hello_world():
     #return 'Hello World'
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0',port=3361,debug=True)
+   app.run(host='0.0.0.0',port=8080,debug=True)
